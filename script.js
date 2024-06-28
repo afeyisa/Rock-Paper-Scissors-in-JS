225a2d740a477b87660d49fefbfb7b32ba1c5f33
afeyisa <kenenisafeyisa@gmail.com>
2024-06-28 15:19:51 +0300

    play function replaced with selection button's event listener

--- script.js
+++ script.js
@@ -3,8 +3,38 @@
 
 const computerChoices = ["rock","scissors","paper"];
 
-let humanScore      = 0 ;
-let computerScore   = 0 ;
+let humanScore      = 0;
+let computerScore   = 0;
+let round           = 0;
+
+
+
+const body = document.getElementById("body");
+// wjere human score get displayed
+const human = document.createElement("div");
+const humanScoreNode = document.createTextNode("you score "+humanScore);
+human.appendChild(humanScoreNode);
+human.setAttribute("style", " padding:10px; border-radius: 4px; margin-bottom:10px; height: 40px; width: 80%;  background: green;");
+
+
+// where computer score get displayed
+const computer = document.createElement("div");
+const computerScoreNode = document.createTextNode(`computer score `+computerScore);
+computer.appendChild(computerScoreNode);
+computer.setAttribute("style","padding:10px; border-radius: 4px; margin-bottom:10px; height: 40px; width: 80%;  background: green;");
+
+//where message get displayed
+const message = document.createElement("div");
+const messageNode = document.createTextNode(`message`);
+message.appendChild(messageNode);
+message.setAttribute("style","padding:10px; border-radius: 4px; margin-bottom:100px; height: 40px; width: 80%;  background: green;");
+
+
+
+const bodys_child = document.getElementById("buttons");
+body.insertBefore(human,bodys_child);
+body.insertBefore(computer,bodys_child);
+body.insertBefore(message,bodys_child);
 
 
 function getComputerChoice (){
@@ -14,94 +44,123 @@ function getComputerChoice (){
 }
 
 
-function getHumanChoice (){
-   return (prompt('enter value'));
+function playRound(humanChoice,computerChoice){
+
+            if(humanChoice){
+                humanChoice = humanChoice.toLowerCase();
+
+            }
+
+        // between rock and paper
+            if(humanChoice == "paper" && computerChoice == "rock")
+            {
+                humanScore ++;
+                humanScoreNode.nodeValue = "your score "+humanScore;
+
+                messageNode.nodeValue =" you win paper beats rock";
+            }
+
+
+
+            else if(humanChoice == "rock" && computerChoice == "paper")
+            {
+                    computerScore ++;
+                    computerScoreNode.nodeValue = `computer score `+computerScore;
+                    messageNode.nodeValue = " you lose paper beats rock";
+            }
+
+
+        // between rock and scissors
+            else if(humanChoice == "rock" && computerChoice == "scissors")
+            {
+                humanScore ++;
+                humanScoreNode.nodeValue = "your score "+humanScore;
+                messageNode.nodeValue = " you win rock beats scissors";
+
+            }
+
+
+            else if(humanChoice == "scissors" && computerChoice == "rock")
+            {
+                computerScore ++;
+                computerScoreNode.nodeValue = `computer score `+computerScore;
+                messageNode.nodeValue = " you lose rock beats scissors";
+            
+            }
+
+
+        // between  paper and scissors
+            else if(humanChoice == "paper" && computerChoice == "scissors")
+            {
+                computerScore++;
+                computerScoreNode.nodeValue = `computer score `+computerScore;
+
+                 messageNode.nodeValue= "you lose  scissors beats paper";
+
+            }
+
+            else if(humanChoice == "scissors" && computerChoice == "paper")
+                {
+                    humanScore++;
+                    messageNode.nodeValue = "you win  scissors beats paper";
+                    humanScoreNode.nodeValue = "your score "+humanScore;
+
+                }
+
+
+        // similar choice or un known human input
+        else if(humanChoice == computerChoice){
+            messageNode.nodeValue="similar choices";
+        }
+        // if user entered wrong word
+        else{
+            messageNode.nodeValue="human choice "+humanChoice;
+            messageNode.nodeValue='computer choice '+computerChoice;
+            messageNode.nodeValue="comparison failure"
+        }
+         
 }
 
 
+const buttons = document.querySelectorAll(".humanSelection");
+buttons.forEach((button) => { 
+    button.addEventListener('click',(e)=>{
+        
+        playRound(e.target.value, getComputerChoice());
+        round++;
 
+        if (round===5){
+            if(humanScore > computerScore){
+                messageNode.nodeValue = `YOU ARE THE WINNER! `
+            }
+            else if(humanScore == computerScore){
+                messageNode.nodeValue = `NO WINNER!`
+            }
+            else{
+                messageNode.nodeValue = `YOU LOSE! `
+            }
 
-function playRound(humanChoice,computerChoice){
-    if(humanChoice){
-        humanChoice = humanChoice.toLowerCase();
+            // create replay button 
+            const btn = document.getElementById("buttons");
+            const replayButton = document.createElement('button');
+            replayButton.className = 'humanSelection'
 
-    }
+            replayButton.addEventListener('click',()=>{
+                window.location.reload();
+            });
 
-// between rock and paper
-    if(humanChoice == "paper" && computerChoice == "rock")
-    {
-        humanScore ++;
-        console.log(" you win paper beats rock");
-    }
-
-
-
-    else if(humanChoice == "rock" && computerChoice == "paper")
-    {
-            computerScore ++;
-            console.log(" you lose paper beats rock");
-    }
-
-
-// between rock and scissors
-    else if(humanChoice == "rock" && computerChoice == "scissors")
-    {
-        humanScore ++;
-        console.log(" you win rock beats scissors");
-
-    }
-
-
-    else if(humanChoice == "scissors" && computerChoice == "scissors")
-    {
-        computerScore ++;
-        console.log(" you lose rock beats scissors");
-    
-    }
-
-
-// between  paper and scissors
-    else if(humanChoice == "paper" && computerChoice == "scissors")
-    {
-        computerScore++;
-        console.log("you lose  scissors beats paper");
-
-    }
-
-    else if(humanChoice == "scissors" && computerChoice == "paper")
-        {
-            humanScore++;
-            console.log("you lose  scissors beats paper");
-    
+            const btnText = document.createTextNode(`replay`);
+            replayButton.appendChild(btnText);
+            btn.replaceChildren(replayButton)
         }
 
+        
+    });
 
-// similar choice or un known human input
-   else if(humanChoice == computerChoice){
-    console.log("similar choices")
-   }
-   else{
-    console.log("comparison failure")
-   }
+});
 
 
 
-    
-}
 
-// 5 round game
-function playGame(){
-
-    for(i = 0; i < 5; i++){
-        let humanSelection    = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    
-    }
-    console.log("computer score "+computerScore,"human score "+humanScore)
-
-
-}
-
-playGame();
+   
 
